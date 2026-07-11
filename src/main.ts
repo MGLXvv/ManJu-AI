66c7d629c0fa9e8312406a994964f333f03a3ce7
MGLXvv <155457106+MGLXvv@users.noreply.github.com>
2026-07-11 20:59:03 +0800

    feat: release media object URLs on application unload

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { i18n } from './i18n'
 import { router } from './router'
+import { mediaBlobRepository } from './services/media'
 import { pinia } from './stores'
 import './assets/iconfont/dist-iconfont/manju-icons.css'
 import './styles/index.scss'
+
+if (typeof window !== 'undefined') {
+  window.addEventListener('beforeunload', () => mediaBlobRepository.revokeObjectUrls())
+}
 
 createApp(App).use(pinia).use(i18n).use(router).mount('#app')

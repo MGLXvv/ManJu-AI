d0389ba4b06e7032183d718c870401b000138040
MGLXvv <155457106+MGLXvv@users.noreply.github.com>
2026-07-12 13:38:31 +0800

    feat: mount global runtime recovery panel

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <RouterView />
   <GlobalLoadingOverlay />
   <GlobalToastStack />
+  <GlobalRuntimeErrorPanel />
 </template>
 
 <script setup lang="ts">
 import GlobalLoadingOverlay from '@/components/app/GlobalLoadingOverlay.vue'
+import GlobalRuntimeErrorPanel from '@/components/app/GlobalRuntimeErrorPanel.vue'
 import GlobalToastStack from '@/components/app/GlobalToastStack.vue'
 </script>
